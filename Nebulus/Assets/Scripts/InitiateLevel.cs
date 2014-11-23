29aad2da79c09f1850caeb39d994b332d91f5ae9
Plamen Popov <disulfiram@users.noreply.github.com>
2014-11-23 18:31:43 +0200

    Started work on level2 design, added script for level initiation(work in progress)

---
+++ Nebulus/Assets/Scripts/InitiateLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitiateLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        GameObject[] levelPlatforms = GameObject.FindGameObjectsWithTag("Platforms");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
