0b68dc7b6f1458a497e6deb90aeb3ba0ea2f0dc8
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2014-12-07 01:07:48 +0200

    Menu scene - added SettingsPanel animations, SettingsPanelExitButton.

--- Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
+++ Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
@@ -6,12 +6,23 @@ public class UIManager : MonoBehaviour {
     public Animator playButton;
     public Animator settingsButton;
     public Animator aboutButton;
+    public Animator settingsPanel;
 
     public void OpenSettings()
     {
         playButton.SetBool("isHidden", true);
         settingsButton.SetBool("isHidden", true);
         aboutButton.SetBool("isHidden", true);
+
+        settingsPanel.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        playButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        aboutButton.SetBool("isHidden", false);
+        settingsPanel.SetBool("isHidden", true);
     }
 
 	public void StartGame(string sceneName) 
