ab3c06596a2dff7f8e04daaac73a3bb15a06f044
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2014-12-06 22:03:39 +0200

    Added animations for Play, Settings and About buttons in Menu scene.

--- Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
+++ Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
+    public Animator playButton;
+    public Animator settingsButton;
+    public Animator aboutButton;
+
+    public void OpenSettings()
+    {
+        playButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        aboutButton.SetBool("isHidden", true);
+    }
+
 	public void StartGame(string sceneName) 
     {
         Application.LoadLevel(sceneName);
