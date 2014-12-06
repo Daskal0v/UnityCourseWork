830b66e96d9c9eb48a5f4996000c95be18f90e6b
Plamen Popov <disulfiram@users.noreply.github.com>
2014-12-06 13:47:43 +0200

    Updated portal script. To use: Tag all portal or a child of theirs as "Portal" *the tagged GameObject needs to have accessible coordinates.* Attach script Portal.cs to all portals and link them. There is an exitPortal public property, so you will need to link each portal with it's exit one. So far the script has hard coded the tower radius and the character path radius but this can be fixed in future(if needed).

--- Nebulus/Assets/Scripts/Portal.cs
+++ Nebulus/Assets/Scripts/Portal.cs
@@ -20,6 +20,10 @@ public class Portal : MonoBehaviour
     {
         Debug.Log("Transport!!!");
         GameObject player = GameObject.Find("Character");
-        player.gameObject.BroadcastMessage("Transport", exitPortal.transform.position);
+        //Vector3 exitLocation = Vector3.Project(Vector3.zero, exitPortal.transform.position);
+        //Math says this should be 1.083333f but pogo spawns way too close in that case
+        float towerRadiusToPathRadius = 1.15f;
+        Vector3 exitLocation = new Vector3(towerRadiusToPathRadius * exitPortal.transform.position.x, 1f * exitPortal.transform.position.y, towerRadiusToPathRadius * exitPortal.transform.position.z);
+        player.gameObject.BroadcastMessage("Transport", exitLocation);
     }
 }
