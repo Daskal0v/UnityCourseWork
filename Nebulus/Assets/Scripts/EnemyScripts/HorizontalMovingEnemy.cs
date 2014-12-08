396522e12dbbea74b213b3e88345ad41831cd2cc
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2014-12-08 23:58:50 +0200

    HorizontalMovingEnemy script - first part

---
+++ Nebulus/Assets/Scripts/EnemyScripts/HorizontalMovingEnemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalMovingEnemy : MonoBehaviour {
+
+    //private Vector3 movingDirection = Vector3.right;
+    //public float upperPosition;
+    //public float lowerPosition;
+    //public float speed = 2;
+
+    //private float radiusA = 5.5f;
+    //private float radiusB = 5.5f;
+    //private float angle;
+    //Vector3 center;
+    //float translation;
+
+    //// Use this for initialization
+    //void Start () {
+	
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+    //    center = new Vector3(0f, transform.position.y, 0f);
+
+    //    //.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+    //    gameObject.transform.Translate(movingDirection * Time.smoothDeltaTime * speed);
+    //    if (gameObject.transform.position.y < lowerPosition)
+    //    {
+    //        movingDirection = Vector3.up;
+    //    }
+
+    //    else if (gameObject.transform.position.y > upperPosition)
+    //    {
+    //        movingDirection = Vector3.down;
+    //    }
+    //}
+
+    public float radius = 10f;
+    public float speed = 1f;
+    public bool offsetIsCenter = true;
+    public Vector3 offset;
+
+    void Start()
+    {
+        if (offsetIsCenter)
+        {
+            offset = transform.position;
+        }
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(
+                    (radius * Mathf.Cos(Time.time * speed)) + offset.x,
+                    offset.y,
+                    (radius * Mathf.Sin(Time.time * speed)) + offset.z);
+    }
+}
---
+++ Nebulus/Assets/Scripts/EnemyScripts/LizardScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LizardScript : MonoBehaviour {
+    bool isLive = true;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLive)
+        {
+            transform.gameObject.SetActive(false);
+        }
+        
+	}
+    void OnCollisionEnter(Collision EnemyCollision)
+    {
+        if (EnemyCollision.gameObject.name == "Bullet")
+        {
+            isLive = false;
+        }
+    }
+    
+}
