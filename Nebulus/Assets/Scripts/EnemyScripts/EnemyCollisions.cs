c864cd32a9285aca2b82d4fb8a9293a6f16039e4
kkoceva <k_koceva@yahoo.com>
2014-12-09 19:06:32 +0200

    "EnemyCollisions script" separated from "EnemyPingPong" script

---
+++ Nebulus/Assets/Scripts/EnemyScripts/EnemyCollisions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollisions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision playerCollision)
+    {
+        if (playerCollision.gameObject.tag == "Player")
+        {
+            playerCollision.gameObject.transform.position += new Vector3(2f, -1f * Time.deltaTime, 2f);
+        }
+    }
+}
--- Nebulus/Assets/Scripts/EnemyScripts/EnemyPingPong.cs
+++ Nebulus/Assets/Scripts/EnemyScripts/EnemyPingPong.cs
@@ -7,8 +7,6 @@ public class EnemyPingPong : MonoBehaviour {
     public float upperPosition;
     public float lowerPosition;
     public float speed;
-    public float pushedPositionX;
-    public float pushedPositionZ;
 
 	// Use this for initialization
     void Start()
@@ -19,23 +17,14 @@ public class EnemyPingPong : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         gameObject.transform.Translate(movingDirection * Time.smoothDeltaTime*speed);
+
         if (gameObject.transform.position.y < lowerPosition)
         {
             movingDirection = Vector3.up;
         }
-
         else if (gameObject.transform.position.y > upperPosition)
         {
             movingDirection = Vector3.down;
         }
 	}
-
-    void OnCollisionEnter(Collision playerCollision)
-    {
-        if (playerCollision.gameObject.tag == "Player")
-        {
-            playerCollision.gameObject.transform.position += new Vector3(2f, -1f * Time.deltaTime, 2f);
-        }
-    }
-
 }
