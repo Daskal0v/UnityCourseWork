bf1596cd0b0abfdbdcc98e09098a28584e2e929a
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2014-12-09 23:26:41 +0200

    HorizontalMovingEnemy script - fully finished

--- Nebulus/Assets/Scripts/EnemyScripts/HorizontalMovingEnemy.cs
+++ Nebulus/Assets/Scripts/EnemyScripts/HorizontalMovingEnemy.cs
@@ -3,63 +3,21 @@ using System.Collections;
 
 public class HorizontalMovingEnemy : MonoBehaviour {
 
-    //private Vector3 movingDirection = Vector3.right;
-    //public float upperPosition;
-    //public float lowerPosition;
-    //public float speed = 2;
-
-    //private float radiusA = 5.5f;
-    //private float radiusB = 5.5f;
-    //private float angle;
-    //Vector3 center;
-    //float translation;
-
-    //// Use this for initialization
-    //void Start () {
-	
-    //}
-	
-    //// Update is called once per frame
-    //void Update () {
-    //    center = new Vector3(0f, transform.position.y, 0f);
-
-    //    //.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
-    //    gameObject.transform.Translate(movingDirection * Time.smoothDeltaTime * speed);
-    //    if (gameObject.transform.position.y < lowerPosition)
-    //    {
-    //        movingDirection = Vector3.up;
-    //    }
-
-    //    else if (gameObject.transform.position.y > upperPosition)
-    //    {
-    //        movingDirection = Vector3.down;
-    //    }
-    //}
-
-    public float radius = 10f;
     public float speed = 1f;
-    public bool offsetIsCenter = true;
-    public Vector3 offset;
-    float sign = 1;
+    private float sign = 1;
 
     void Start()
     {
-        if (offsetIsCenter)
-        {
-            offset = transform.position;
-        }
     }
 
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.up, 10 * Time.deltaTime * sign);
-    
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime * sign);
     }
 
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("OnCollisionEnter");
-        sign = -1;
-
+        sign = -1*sign;
     }
 }
