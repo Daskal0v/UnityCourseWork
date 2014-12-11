352ac8bb9f0ad205335b545110d2da5ec827e98c
kkoceva <k_koceva@yahoo.com>
2014-12-11 16:16:40 +0200

    Add scripts("LevelLoad", "DestroyablePlatform")

--- Nebulus/Assets/Scripts/DestroyablePlatform.cs
+++ Nebulus/Assets/Scripts/DestroyablePlatform.cs
@@ -3,12 +3,28 @@ using System.Collections;
 
 public class DestroyablePlatform : MonoBehaviour {
 
+    bool isDestroyable=false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDestroyable==true)
+        {
+             Destroy(gameObject);
+        }
+
+    }
+
     void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag=="Destroyable")
+        if (collision.gameObject.tag=="Player")
         {
-            Destroy(collision.gameObject);
-            
+            isDestroyable = true;
         }
     }
 }
---
+++ Nebulus/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoad : MonoBehaviour {
+
+    private bool isLoading = false;
+    private string[] scenes;
+    public float timer;
+    public float secondsToLoadNextLevel = 7f;
+
+	// Use this for initialization
+	void Start () {
+        scenes = new string[4]{"level1","level2","level3","level4"};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > secondsToLoadNextLevel)
+        {
+            timer = 0;
+        }
+
+        if (timer == 0 && isLoading == true)
+        {
+            int scenesCount = 0;
+
+            while (scenesCount < scenes.Length && scenesCount>=0)
+            {
+                scenesCount++;
+                if (Application.loadedLevelName == scenes[scenesCount - 1] && scenes.Length > scenesCount)
+                {
+                    Application.LoadLevel(scenes[scenesCount]);
+                }
+                else if (Application.loadedLevelName == scenes[scenes.Length - 1])
+                {
+                    Application.LoadLevel("menu");
+                }
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isLoading = true;
+        }
+    }
+}
