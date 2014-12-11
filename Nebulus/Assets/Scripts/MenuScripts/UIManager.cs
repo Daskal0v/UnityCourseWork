0b929822cefd6f8b804be06b58df808d6c73633e
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2014-12-11 22:30:44 +0200

    Added About Panel + animations - Menu scene

--- Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
+++ Nebulus/Assets/Scripts/MenuScripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour {
     public Animator settingsButton;
     public Animator aboutButton;
     public Animator settingsPanel;
+    public Animator teamPanel;
 
     public void OpenSettings()
     {
@@ -23,6 +24,24 @@ public class UIManager : MonoBehaviour {
         settingsButton.SetBool("isHidden", false);
         aboutButton.SetBool("isHidden", false);
         settingsPanel.SetBool("isHidden", true);
+    }
+
+    public void OpenAbout()
+    {
+        playButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        aboutButton.SetBool("isHidden", true);
+
+        teamPanel.SetBool("isHidden", false);
+    }
+
+    public void CloseAbout()
+    {
+        playButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        aboutButton.SetBool("isHidden", false);
+
+        teamPanel.SetBool("isHidden", true);
     }
 
 	public void StartGame(string sceneName) 
