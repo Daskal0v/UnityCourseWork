235917bf49271f1591d84dff3caa3e7f88d72364
Plamen Popov <disulfiram@users.noreply.github.com>
2014-12-13 00:13:55 +0200

    Somewhat finalish Level2 submit. EnemyCollision.cs added option to select distance for enemies to push player away (it calculate coordinates by itself)

--- Nebulus/Assets/Scripts/EnemyScripts/EnemyCollisions.cs
+++ Nebulus/Assets/Scripts/EnemyScripts/EnemyCollisions.cs
@@ -4,12 +4,23 @@ using System.Collections;
 public class EnemyCollisions : MonoBehaviour {
     public float playerPushedX;
     public float playerPushedZ;
+    public float pushDistance;
 
     void OnCollisionEnter(Collision playerCollision)
     {
         if (playerCollision.gameObject.tag == "Player")
         {
-            playerCollision.gameObject.transform.position += new Vector3(playerPushedX, -1f * Time.deltaTime, playerPushedZ);
+            if (pushDistance != 0)
+            {
+                playerCollision.gameObject.transform.position = new Vector3(
+                    playerCollision.gameObject.transform.position.x * pushDistance, 
+                    playerCollision.gameObject.transform.position.y, 
+                    playerCollision.gameObject.transform.position.z * pushDistance);
+            }
+            else
+            {
+                playerCollision.gameObject.transform.position += new Vector3(playerPushedX, -1f * Time.deltaTime, playerPushedZ);
+            }
         }
     }
 }
