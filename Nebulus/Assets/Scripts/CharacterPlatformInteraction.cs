7e145e6a83d487d43b269900f67153cfcf83f456
kkoceva <k_koceva@yahoo.com>
2014-12-12 02:52:57 +0200

    "DestroyablePlatform" script renamed to "CharacterPlatformInteraction" script

---
+++ Nebulus/Assets/Scripts/CharacterPlatformInteraction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterPlatformInteraction : MonoBehaviour {
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Destroyable")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
